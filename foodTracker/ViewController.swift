9c73612d77d240267f0a34039789ee75fc886a49
Tyler Donohue <tydon@tylers-macbook-pro-2.local>
2018-08-28 08:55:51 -0600

    Initial Commit

---
+++ foodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodTracker
+//
+//  Created by Tyler Donohue on 8/28/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

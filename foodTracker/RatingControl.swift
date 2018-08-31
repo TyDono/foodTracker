4e7ecb0ef2b0bef97fd84ccfe15dd66283c0e7c1
Tyler Donohue <tydon@tylers-macbook-pro-2.local>
2018-08-31 09:49:28 -0600

    i did it :D i guess

--- foodTracker/RatingControl.swift
+++ foodTracker/RatingControl.swift
@@ -8,21 +8,53 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
 
     //MARK: Initialization
     
    override init(frame: CGRect) {
     super.init(frame: frame)
+    setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed :D Bob Ross will now claim your soul!")
     }
     
     //MARK: Private Methods
     
     private func setupButtons() {
+        
+        for _ in 0..<5 {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action:
+    #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+        }
         
     }
 

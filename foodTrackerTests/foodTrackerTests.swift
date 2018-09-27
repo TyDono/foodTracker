0a118cf28ebeacc468a5af016d185127f66edf64
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-27 09:27:31 -0600

    added meal.swift and added data in test.swift

--- foodTrackerTests/foodTrackerTests.swift
+++ foodTrackerTests/foodTrackerTests.swift
@@ -11,26 +11,20 @@ import XCTest
 
 class foodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    //MARK: Meal CLass Tests
+    // Confirm that Meal initializer returns a Meal object when passed valid parameteres.
+    func testMealInitializationSucceeds() {
+        // Negative Rating
+        let zeroRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNotNil(zeroRatingMeal)
+        // Empty String
+        let positiveRatingMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNotNil(positiveRatingMeal)
+        // Confirm that the Meal initializer returns nil when passed a negsative rating or an empty name.
+        func testMealInitializationFails() {
+            
         }
+        
     }
-    
 }

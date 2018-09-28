31bf1c02cf115a8f0db7bf99f98b3b4edea4388e
Tyler Donohue <donohue_tyler@yahoo.com>
2018-09-28 09:36:14 -0600

    adding meal. swift, meal.viewtablecontroller, mealviewtable, and view table in main

---
+++ foodTracker/MealTableViewController.swift
@@ -0,0 +1,118 @@
+//
+//  MealCollectionViewController.swift
+//  foodTracker
+//
+//  Created by Tyler Donohue on 9/28/18.
+//  Copyright © 2018 Tyler Donohue. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MealCollectionViewController: UICollectionViewController {
+    
+    //MARK: Properties
+    
+    var meals = [Meal]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the sample data.
+        loadSampleMeals()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        // Configure the cell
+        
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+    
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
+    
+    //MARK: Private Mthofs
+    
+    private func loadSampleMeals() {
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "we are number 1", photo: photo1, rating: 4) else {
+            fatalError("THROW IT ON HIM NOT ME!1")
+        }
+        
+        guard let meal2 = Meal(name: "now look at this net", photo: photo2, rating: 5) else {
+            fatalError("THROW IT ON HIM NOT ME!2")
+        }
+        
+        guard let meal3 = Meal(name: "Robbie is dead", photo: photo3, rating: 3) else {
+            fatalError("THROW IT ON HIM NOT ME!3")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+    
+}
